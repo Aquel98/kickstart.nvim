0af90b477a6cf14d43e42e8d3b4f8bcaa3d7aa06
Axel Ståhlbom <cass981028@hotmail.com>
2025-12-28 17:05:23 +0100

    Add plugin for writing/reading with sudo

---
+++ lua/custom/plugins/suda.lua
@@ -0,0 +1,8 @@
+-- suda is a plugin to allow read or write to files with sudo command.
+-- https://github.com/lambdalisue/vim-suda
+
+return {
+  'lambdalisue/vim-suda',
+  version = '*',
+  lazy = false,
+}
