068234dd86045184011d2c9122b90f48589d4148
Axel Stahlbom <axel.stahlbom@epiroc.com>
2026-01-09 22:29:25 +0100

    Change how file names are displayed in telescope

--- init.lua
+++ init.lua
@@ -464,6 +464,10 @@ require('lazy').setup({
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
+        defaults = {
+          path_display = { 'truncate', 'filename_first' },
+        },
+
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
