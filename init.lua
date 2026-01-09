ac4238e64e605be2889d384507fe4c91fcf55e75
Axel Stahlbom <axel.stahlbom@epiroc.com>
2026-01-09 22:31:36 +0100

    Add keybind for listing commits of a file

--- init.lua
+++ init.lua
@@ -504,6 +504,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', 'glc', builtin.git_bcommits, { desc = '[G]it [L]ist [C]ommits' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
