da96046e63ac6b9e83a2167e1bace9f1de4832c5
Axel Stahlbom <axel.stahlbom@epiroc.com>
2026-01-09 22:33:01 +0100

    Add keybind for switching between source and header

--- init.lua
+++ init.lua
@@ -641,6 +641,9 @@ require('lazy').setup({
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          -- Switch between header and source file
+          map('grs', vim.cmd.LspClangdSwitchSourceHeader, 'Switch Source/Header')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
