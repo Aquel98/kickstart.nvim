3893395a5fa361ec7e2cb7b3298c853f26065fde
Axel Ståhlbom <cass981028@hotmail.com>
2026-01-18 17:47:17 +0100

    Change Telescope search help to Snacks search help

--- init.lua
+++ init.lua
@@ -400,6 +400,13 @@ require('lazy').setup({
         desc = 'Diagnostics',
       },
       {
+        '<leader>sh',
+        function()
+          Snacks.picker.help()
+        end,
+        desc = 'Help Pages',
+      },
+      {
         '<leader>sr',
         function()
           Snacks.picker.resume()
@@ -623,7 +630,7 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       -- vim.keymap.set('n', '<leader>sf', function()
       --   builtin.find_files {
