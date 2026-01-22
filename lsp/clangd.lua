34c425e1bb69bb4e6420ecb715cadb81cee73801
Axel Ståhlbom <cass981028@hotmail.com>
2026-01-22 01:05:11 +0100

    Refactoring LSP plugins and stuff

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -3,5 +3,8 @@ return {
     'clangd',
     -- '--query-driver=' .. vim.g.clangd_query_driver,
     '--query-driver=/opt/sdks/**/*linux-g++',
+    '--clang-tidy',
   },
+  root_markers = { '.clangd', 'compile_commands.json' },
+  filetypes = { 'c', 'cpp' },
 }
