fb06d557fe31aed092f95049b70dab2dfe2b71c7
Axel Ståhlbom <cass981028@hotmail.com>
2026-02-01 22:13:04 +0100

    Reformat files and update lua_ls.lua and clangd.lua

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,21 +1,41 @@
-return {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = {
-    '.emmyrc.json',
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
-  settings = {
-    lua = {
-      codeLens = { enable = true },
-      hint = { enable = true, semicolon = 'Disable' },
-    },
-  },
+local config = {
+   cmd = { "lua-language-server" },
+   filetypes = { "lua" },
+   root_markers = {
+      ".emmyrc.json",
+      ".luarc.json",
+      ".luarc.jsonc",
+      ".luacheckrc",
+      ".stylua.toml",
+      "stylua.toml",
+      "selene.toml",
+      "selene.yml",
+      ".git",
+   },
+   settings = {
+      Lua = {
+         codeLens = { enable = true },
+         hint = { enable = true, semicolon = "Disable" },
+         completion = {
+            callSnippet = "Replace",
+         },
+         -- Formatting through 'format' doesn't seem to work for the formatter used by lua_ls (EmmyLuaCodeStyle), however using the .editorconfig file seeems to work. The editor config only applies to the current project.
+         diagnostics = { globals = { "vim" } },
+      },
+   },
+   capabilities = {
+      {
+         textdocument = {
+            completion = {
+               completionItem = {
+                  snippetSupport = true,
+               },
+            }
+         }
+      },
+   },
 }
+
+vim.tbl_deep_extend("force", require("lsp_capabilities"), config.capabilities)
+
+return config
